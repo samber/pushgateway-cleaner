502d3462b08a830e22f2b8b0f688c6a41bf5e4c9
Samuel Berthe <dev@samuel-berthe.fr>
2022-05-25 13:54:30 +0200

    change accuracy to minutes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const logger = require('./logger')
 const main = async () => {
     try {
         const groups = await getMetricsGroups(PUSHGATEWAY_URL);
-        const rangeTimeForExpiredGroup = process.env['TTL_HOURS'] * 1000 * 60 * 60;
+        const rangeTimeForExpiredGroup = process.env['TTL_MINUTES'] * 1000 * 60;
         const expiredGroups = getExpiredGroups(groups, rangeTimeForExpiredGroup);
         const urls = await removeExpiredGroups(expiredGroups, PUSHGATEWAY_URL);
         logger.info(`Removed ${urls.length} groups of metrics from pushgateway`);
@@ -14,7 +14,7 @@ const main = async () => {
         if (SLACK_WEBHOOK_URL) {
             await notifySlack(expiredGroups, SLACK_WEBHOOK_URL)
         }
-    } catch(error) {
+    } catch (error) {
         logger.error(error.message);
     }
 }
